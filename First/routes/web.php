2d5099d0f1adb2442f3990649b622423e0d5ad12
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-09-15 21:10:57 +0600

    laravel practice

--- First/routes/web.php
+++ First/routes/web.php
@@ -10,4 +10,16 @@ Route::get('/about', function () {
     echo "This is about page";
 });
 
+Route::get('/contact', function () {
+echo "This is contact page";
+});
+
+Route::get('/blog', function () {
+    return view('welcome');
+});
+
+
+Route::get('/protfolio', function () {
+    return view('welcome');
+});
 
