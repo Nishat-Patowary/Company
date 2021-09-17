e3b048c7f9a8eccf706d0909a2a829426e449845
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-09-17 20:53:37 +0600

    laravel practice

--- project-1/routes/web.php
+++ project-1/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use app\http\Controllers\ContactController;
 use app\Models\user;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -31,5 +32,6 @@ Route::get('/contact', function () {
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     $users = User::all();
+    $users= DB::table("users")->get();
     return view('dashboard',compact("users"));
 })->name('dashboard');
