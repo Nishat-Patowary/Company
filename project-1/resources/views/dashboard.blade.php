24e20b8ac5d0e4c2e633ececddd05092af8c65cc
Tanvirul HQ NIshat <nishatpatwary2001@gmail.com>
2021-09-17 20:05:20 +0600

    laravel practice

--- project-1/resources/views/dashboard.blade.php
+++ project-1/resources/views/dashboard.blade.php
@@ -1,10 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+         Hi....<b>{{ Auth::user()->name }}</b>
+         <b style="float: right;">Total users  
+         <span>{{ count($users) }}</span>
+        </b>
         </h2>
     </x-slot>
-
+ 
     <div class="py-12">
     <div class="container">
         <div class="row">
